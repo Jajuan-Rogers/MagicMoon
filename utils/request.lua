4e2df3215b422aef0a2aadbf8828e03696a8208e
Ben <Bjornhephaestuts@proton.me>
2026-04-27 21:08:16 -0700

    table concat bug, WIP!

--- utils/request.lua
+++ utils/request.lua
@@ -88,40 +88,51 @@ function M.fetch_scryfall_deck(txt_fp)
 	end
 
 	local all_ids = deck_data.identifiers
-	local first_req_chunk = { identifiers = table.move(deck_data.identifiers, 1, 74, 1, {}) }
-	-- for k, v in pairs(first_req_chunk.identifiers) do
-	-- 	print(k, v.name)
-	-- end
-	-- os.exit()
+	local chunk_1 = { identifiers = table.move(deck_data.identifiers, 1, 75, 1, {}) }
+	local chunk_2 = { identifiers = table.move(all_ids, 76, #all_ids, 1, {}) }
+	local chunks = { chunk_1, chunk_2 }
 
-	local request_body = json.encode(first_req_chunk)
-	local response_chunks = {}
-	local r, code, headers, status = https.request({
-		url = api_url,
-		method = "POST",
-		headers = {
-			["Content-Type"] = "application/json",
-			["Content-Length"] = tostring(#request_body),
-			["User-Agent"] = "Magically/1.0",
-			["Accept"] = "application/json",
-		},
-		source = ltn12.source.string(request_body),
-		sink = ltn12.sink.table(response_chunks),
-	})
-
-	if code == 200 then
-		local final_response = table.concat(response_chunks)
-		local data = json.decode(final_response)
-		print("successfully recieved deck")
-		return data
-	else
-		print("ERROR: ", tostring(code), r, tostring(status))
-		print(table.concat(response_chunks))
-		return nil
+	local final_data
+	for i = 1, #chunks do
+		local request_body = json.encode(chunks[i])
+		local response_chunks = {}
+		local r, code, headers, status = https.request({
+			url = api_url,
+			method = "POST",
+			headers = {
+				["Content-Type"] = "application/json",
+				["Content-Length"] = tostring(#request_body),
+				["User-Agent"] = "Magically/1.0",
+				["Accept"] = "application/json",
+			},
+			source = ltn12.source.string(request_body),
+			sink = ltn12.sink.table(response_chunks),
+		})
+		if code == 200 then
+			local final_response = table.concat(response_chunks)
+			final_data = json.decode(final_response)
+			print("successfully recieved deck")
+		else
+			print("ERROR: ", tostring(code), r, tostring(status))
+			print(table.concat(response_chunks))
+		end
+    if i == 1 then
+      chunk_1 = final_data
+    else
+      chunk_2 = final_data
+    end
 	end
+	final_data = table.move(chunks[2], 1, #chunks[2], #chunks[1] + 1, chunk_1)
+
+	for k, v in pairs(final_data.data) do
+		print(k, v.name)
+	end
+	os.exit()
+	return final_data
 end
 
 local d = M.fetch_scryfall_deck("test_files/blood_rites.txt")
+
 for a, b in pairs(d.data) do
 	print(a, b.name, b.set, b.finish)
 end
